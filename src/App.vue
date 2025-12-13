6492d9def4b1a5ae12247b75f107ff31ea350bee
vitor <mistersonic7@gmail.com>
2025-12-13 20:45:13 -0300

    style changes

--- src/App.vue
+++ src/App.vue
@@ -7,21 +7,21 @@ const selectedHeroes = ref<string[]>([])
 </script>
 
 <template>
-  <div class="antialiased flex bg-black text-white p-2 h-dvh w-full overflow-auto">
+  <div class="flex bg-black text-white p-2 h-dvh w-full antialiased">
     <!-- companion list section -->
-    <div class="w-50">
+    <div class="w-50 overflow-scroll">
       <h2 class="text-center mb-4 text-lg">Companions</h2>
       <CompanionPicker v-model="selectedHeroes" />
     </div>
 
     <!-- card section -->
-    <div class="w-full p-4 border rounded-lg mx-4 flex gap-4 overflow-scroll max-h-dvh 2xl:h-fit">
+    <div class="w-full p-4 border rounded-lg mx-4 flex gap-4 overflow-scroll 2xl:h-fit">
       <HeroCard name="Player" />
       <HeroCard v-for="hero in selectedHeroes" :key="hero" :name="hero" />
     </div>
 
     <!-- party bonuses section -->
-    <div class="w-100">
+    <div class="w-100 overflow-scroll">
       <h2 class="text-center mb-4 text-lg">Party Bonuses</h2>
     </div>
   </div>
